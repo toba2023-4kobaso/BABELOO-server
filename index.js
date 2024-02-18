0842486369956fc9cf34801ff5cc96559b95e913
4geniu <taketubo0714@gmail.com>
2024-02-18 19:05:49 +0900

    logのformatを整えた

--- index.js
+++ index.js
@@ -3,13 +3,9 @@ const server = new Server({
   port: 8000,
   timezone: 'Asia/Tokyo',
 });
-const axios = require('axios').default;
-const { v4: uuidv4 } = require('uuid');
-require('dotenv').config();
 
-let location = "japanwest";
-let endpoint = "https://api.cognitive.microsofttranslator.com/";
-var key = process.env.api_key;
+const axios = require('axios').default;
+const buildRequest = require('./request_builder').buildRequest;
 /*
 axios({
   baseURL: endpoint,
@@ -44,31 +40,15 @@ server.events.on('playerChat', async (event) => {
   
   if (event.sender === '外部') return; // スパムの無限ループを防ぐ
   let message = event.message;
-  server.logger.info(`<${event.sender}> ${event.message}`);
+  server.logger.info(`<${event.sender}> ${message}`);
 
-  axios({
-    baseURL: endpoint,
-    url: '/translate',
-    method: 'post',
-    headers: {
-        'Ocp-Apim-Subscription-Key': key,
-         // location required if you're using a multi-service or regional (not global) resource.
-        'Ocp-Apim-Subscription-Region': location,
-        'Content-type': 'application/json',
-        'X-ClientTraceId': uuidv4().toString()
-    },
-    params: {
-        'api-version': '3.0',
-        'from': 'ja',
-        'to': "en"
-    },
-    data: [{
-        'text': message
-    }],
-    responseType: 'json'
-  }).then(function(response){
-    console.log(response.data[0].translations[0].text);
-    event.world.sendMessage(response.data[0].translations[0].text);
+  axios(
+    buildRequest(message, "ja", "en")
+  ).then( response => {
+
+    let translated = response.data[0].translations[0].text;
+    server.logger.info(`<${event.sender}> ${message} (${translated})`);
+    event.world.sendMessage(`<${event.sender}> ${message}§r§7(${translated})`);
   })
   
   /*
---
+++ request_builder.js
@@ -0,0 +1,31 @@
+const axios = require('axios').default;
+const { v4: uuidv4 } = require('uuid');
+require('dotenv').config();
+
+let location = "japanwest";
+let endpoint = "https://api.cognitive.microsofttranslator.com/";
+var key = process.env.api_key;
+
+exports.buildRequest = (message, from, to) => {
+  return {
+    baseURL: endpoint,
+    url: '/translate',
+    method: 'post',
+    headers: {
+        'Ocp-Apim-Subscription-Key': key,
+         // location required if you're using a multi-service or regional (not global) resource.
+        'Ocp-Apim-Subscription-Region': location,
+        'Content-type': 'application/json',
+        'X-ClientTraceId': uuidv4().toString()
+    },
+    params: {
+        'api-version': '3.0',
+        'from': from,
+        'to': to
+    },
+    data: [{
+        'text': message
+    }],
+    responseType: 'json'
+  }
+}
