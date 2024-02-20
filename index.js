606275a3bb2824d6f5499d76993cfbeacdd33808
4geniu <taketubo0714@gmail.com>
2024-02-21 02:15:30 +0900

    コメントアウトしていた部分を削除

--- index.js
+++ index.js
@@ -6,54 +6,37 @@ const server = new Server({
 
 const axios = require('axios').default;
 const buildRequest = require('./request_builder').buildRequest;
-/*
-axios({
-  baseURL: endpoint,
-  url: '/translate',
-  method: 'post',
-  headers: {
-      'Ocp-Apim-Subscription-Key': key,
-       // location required if you're using a multi-service or regional (not global) resource.
-      'Ocp-Apim-Subscription-Region': location,
-      'Content-type': 'application/json',
-      'X-ClientTraceId': uuidv4().toString()
-  },
-  params: {
-      'api-version': '3.0',
-      'from': 'en',
-      'to': "ja"
-  },
-  data: [{
-      'text': 'I would really like to drive your car around the block a few times!'
-  }],
-  responseType: 'json'
-}).then(function(response){
-  console.log(JSON.stringify(response.data, null, 4));
-})
-*/
 
 server.events.on('serverOpen', () => {
   console.log('open!');
 });
 
-server.events.on('playerChat', async (event) => {
+server.events.on('playerTitle', async (event) => {
   
   if (event.sender === '外部') return; // スパムの無限ループを防ぐ
-  let message = event.message;
+  if (event.sender === 'External') return; // スパムの無限ループを防ぐ
+
+  let messageInfo = JSON.parse(event.message);
+
+  let language = messageInfo.language;
+  let message = messageInfo.message;
   server.logger.info(`<${event.sender}> ${message}`);
 
+  let request = {};
+
+  if (language === "ja") {
+    request = buildRequest(message, "ja", "en");
+  }
+  else {
+    request = buildRequest(message, "en", "ja");
+  }
+
   axios(
-    buildRequest(message, "ja", "en")
+    request
   ).then( response => {
 
     let translated = response.data[0].translations[0].text;
     server.logger.info(`<${event.sender}> ${message} (${translated})`);
     event.world.sendMessage(`<${event.sender}> ${message}§r§7(${translated})`);
   })
-  
-  /*
-  if (event.message === 'ping') {
-    await event.world.sendMessage('Pong!');
-  }
-  */
 });
